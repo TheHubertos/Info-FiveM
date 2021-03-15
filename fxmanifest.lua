5ba05461dfe08ab00cded20d365c27a0a157f40a
TheHubertos <67113860+TheHubertos@users.noreply.github.com>
2021-03-15 12:32:23 +0100

    1

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Info.'
+
+version '1.0'
+
+server_scripts {
+	"sv.lua"
+}
---
+++ sv.lua
@@ -0,0 +1,59 @@
+ESX 			    			= nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+function ExtractIdentifiers(src)
+    local identifiers = {
+        steam = "",
+        ip = "",
+        discord = "",
+        license = "",
+        xbl = "",
+        live = ""
+    }
+
+    for i = 0, GetNumPlayerIdentifiers(src) - 1 do
+        local id = GetPlayerIdentifier(src, i)
+
+        if string.find(id, "steam") then
+            identifiers.steam = id
+        elseif string.find(id, "ip") then
+            identifiers.ip = id
+        elseif string.find(id, "discord") then
+            identifiers.discord = id
+        elseif string.find(id, "license") then
+            identifiers.license = id
+        elseif string.find(id, "xbl") then
+            identifiers.xbl = id
+        elseif string.find(id, "live") then
+            identifiers.live = id
+        end
+    end
+
+    return identifiers
+end
+
+webhook = "https://discord.com/api/webhooks/820940032662568971/Qt1Hg4C77HHNePD9E6KGLSP1awkoDttkAHPRVQYD82ZpXgRqoZI0y7-vC-i8FYpSo2xG"
+
+RegisterServerEvent('wolfi_log')
+AddEventHandler('wolfi_log', function(message, color)
+	PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "ExperienceRP", embeds = {{["color"] = color,["title"] = "Informacje o graczu",["description"] = "".. message .."",["footer"] = {["text"] = "ExperienceRP"},}},}), { ['Content-Type'] = 'application/json' })
+end)
+
+RegisterCommand('info', function(playerId, args, rawCommand)
+	if playerId == 0 then
+		print('Nie możesz użyć tej komendy z poziomu konsoli!')
+	else
+		local target = tonumber(args[1])
+		local xTarget = ESX.GetPlayerFromId(target)
+		local xPlayer = ESX.GetPlayerFromId(playerId)
+		local group = xPlayer.getGroup()
+		local ids = ExtractIdentifiers(target)
+
+		if group ~= 'user' then
+			TriggerClientEvent('esx:showNotification', playerId, "Akcja wykonana pomyślnie!")
+			TriggerEvent('wolfi_log', "**Nick:** "..GetPlayerName(playerId).."\n**Identyfikator:** "..xTarget.getIdentifier().."\n**Discord:** <@"..ids.discord:gsub("discord:", "")..">\n**Licencja: **"..ids.license.."\n**Imię Nazwisko:** ".. xTarget.getName() .."\n**Stan Konta:** ".. xTarget.getAccount('bank').money .."$\n**Stan gotówki:** ".. xTarget.getMoney() .."$\n **Grupa:** "..xTarget.getGroup().."\n**Praca:** "..xTarget.job.label.. " - "..xTarget.job.grade_label.." (".. xTarget.job.grade..")".."\n**Praca2:** "..xTarget.job2.label.. " - "..xTarget.job2.grade_label.." (".. xTarget.job2.grade..") \n**Koordynaty: **"..xPlayer.getCoords(true), "1")
+        else
+            TriggerClientEvent('esx:showNotification', playerId, 'Brak dostępu!')
+        end
+	end
+end, false)
